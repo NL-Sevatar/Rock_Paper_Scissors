6c046f480cf2d739edceed8e649789cba742b057
Oden <Nada>
2021-12-21 10:11:04 -0800

    Added a function to play 5 rounds of the game. added a score function to track wins for both player and the computer then at the end say who won the best of 5

--- script/rps.js
+++ script/rps.js
@@ -5,6 +5,9 @@ const choice = [
     "scissors"
 ];
 
+let playerScore = 0;
+let computerScore = 0;
+
 // Selects the choice randonmly for the computer
 function computerPlay(arr) {
     let random = Math.floor(Math.random()*arr.length);
@@ -16,36 +19,42 @@ function computerPlay(arr) {
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === "rock"){
-        if (computerSelection == "Rock") {
+        if (computerSelection === "rock") {
                 return "Draw!";
         }
         
-        else if (computerSelection == "Paper") {
+        else if (computerSelection === "paper") {
+                computerScore++;
                 return "You lose! Paper beats Rock.";
         }
 
-        else (computerSelection == "scissors"); {
+        else (computerSelection === "scissors"); {
+                playerScore++
                 return "You win! Rock beats Scissors.";
         }
     }
     
     else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
+                playerScore++
                 return "You win! Paper beats Rock.";
         }
         else if (computerSelection === "paper") {
                 return "Draw!";
         }
         else (computerSelection === "scissors"); {
+                computerScore++
                 return "You lose! Scissors beats Rock.";
         }
     }
 
     if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
+                computerScore++
                 return "You lose! Rock beats Scissors";
         }
         else if (computerSelection === "paper") {
+                playerScore++
                 return "You win! Scissors beats Paper.";
         }
 
@@ -59,8 +68,10 @@ function playRound(playerSelection, computerSelection) {
     }
 };
 
-// Player inputs choice for the game
-const playerSelection = prompt("Rock, Paper, or Scissors?", " ");
+function selections() {
+
+    // Player inputs choice for the game
+const playerSelection = prompt("Rock, Paper, or Scissors?", " ".toLowerCase);
     console.log(playerSelection);
 
 // Creating the actual choice of the computer.
@@ -69,8 +80,49 @@ const computerSelection = computerPlay(choice)
 // Test to ensure random selection is working
     console.log(computerSelection);
 
-// Playing a single round
- console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+}
 
+function score() {
+    if (playerScore > computerScore){
+        console.log("Player wins!")
+    }
+    else if (computerScore > playerScore) {
+        console.log("Computer wins!")
+    }
+    else {
+        console.log("Oops! Something went wrong.")
+    }
+}
 
+function game() {
+let i = 0;
+    do {
+        let playerSelection = prompt("Rock, Paper, or Scissors?", " ");
+        let computerSelection = computerPlay(choice);
+        console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+        i++;
+    }
+    while(i <5 )
     
+    return score();
+};
+
+
+
+// Playing a single round
+    
+  /*  // Player inputs choice for the game
+       let playerSelection = prompt("Rock, Paper, or Scissors?", " ");
+       console.log(playerSelection);
+
+    // Creating the actual choice of the computer.
+       let computerSelection = computerPlay(choice)
+
+    // Test to ensure random selection is working
+       console.log(computerSelection);
+
+    // Single Round
+       console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+*/
+// Playing a full 5 rounds
+    game();
