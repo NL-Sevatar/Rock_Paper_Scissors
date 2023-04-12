906f232b5463c8919ea0762b292cba4c858e4481
Sevatar <29562343+NL-Sevatar@users.noreply.github.com>
2023-04-12 12:38:49 -0500

    Returning to TOP I have gone through and cleaned this code up reducing nested if statements in the determining winner

--- script/rps.js
+++ script/rps.js
@@ -1,104 +1,71 @@
-const choice = [
-    "rock",
-    "paper",
-    "scissors"
-];
+function getComputerChoice() {
+    const choice = [
+        "rock",
+        "paper",
+        "scissors"
+    ];
+    let random = Math.floor(Math.random() * choice.length);
+    let computerSelection = choice[random];
+    return computerSelection;
+}
 
 let playerScore = 0;
 let computerScore = 0;
-const battle = document.querySelector('#results')
-const pScore = document.querySelector('#player-score');    
-const cScore = document.querySelector('#computer-score');
+//const battle = document.querySelector('#results')
+//const pScore = document.querySelector('#player-score');    
+//const cScore = document.querySelector('#computer-score');
 
-function computerPlay() {
-    let random = Math.floor(Math.random()*choice.length);
-    let computerSelection = choice[random];
-    return computerSelection;
-};
-
-const buttons = document.querySelectorAll('#pick');
-buttons.forEach((pick) => {pick.addEventListener('click', () => {
-    playerSelection = pick.title;
-    playRound(playerSelection);
-    if (playerScore == 5 || computerScore == 5){
-        declareWinner();
-    }
-    })})
-
-function playRound(playerSelection) { 
-    let computerSelection = computerPlay();
-    
-    if (playerSelection === computerSelection) {
-        battle.textContent = "Draw!";
-        return;
-    }
-    
-    if (playerSelection === "rock"){
-        if (computerSelection === "paper") {
-                computerScore++;
-                compUpdate();
-                battle.textContent = "You lose! Paper beats Rock.";
-                return;
-        }
-
-        else (computerSelection === "scissors"); {
-                playerScore++
-                playerUpdate();
-                battle.textContent = "You win! Rock beats Scissors.";
-                return;
-        }
-    }
-    
-    else if (playerSelection === "paper") {
-        
-        if (computerSelection === "rock") {
-                playerScore++
-                playerUpdate();
-                battle.textContent = "You win! Paper beats Rock.";
-                return;
-        }
-        
-        else (computerSelection === "scissors"); {
-                computerScore++
-                compUpdate();
-                battle.textContent = "You lose! Scissors beats Rock.";
-                return;
-        }
+function determineWinner(playerSelection, computerSelection)
+   {
+    if(playerSelection === computerSelection) {
+        return console.log("tie");
     }
 
-    if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-                computerScore++;
-                compUpdate();
-                battle.textContent = "You lose! Rock beats Scissors";
-                return;
-        }
-        else (computerSelection === "paper"); {
-                playerScore++
-                playerUpdate();
-                battle.textContent = "You win! Scissors beats Paper.";
-                return;
-        }
-    
+    else if (
+            (playerSelection === "rock" && computerSelection === "scissors")  ||
+            (playerSelection === "paper" && computerSelection ==="rock")      ||
+            (playerSelection === "scissors" && computerSelection === "paper") 
+    )
+     {
+        playerScore++;
+        return console.log("player");    
     }
-};
 
-function compUpdate() {    
+    else {
+        computerScore++;
+        return console.log("computer");
+    }
+   }
+
+/* function compUpdate() {    
         cScore.textContent = computerScore;
 }
 function playerUpdate() {
         pScore.textContent = playerScore;
 }
-
+*/
 function declareWinner() {
-    if (playerScore > computerScore) {
+    if (playerScore > computerScore && playerScore === 5) {
         window.alert('You are the victor!');
-        let playerScore = 0;
-        let computerScore = 0;
-        location.reload();
+        //location.reload();
     } 
     else if (computerScore === 5) {
         window.alert('Defeat, may they have mercy on your soul.');
-      location.reload();
+        //location.reload();
+    }
+    else {
+        return console.log("Next Round!")
     }
    }
+
+   function playGame(){
+    do {
+    const playerSelection = prompt("Choose rock, paper, or scissors:").toLowerCase();
+    const computerSelection = getComputerChoice();
+    determineWinner(playerSelection, computerSelection);
+    console.log(`${playerSelection} was your choice and the computer picked ${computerSelection}`);
+    declareWinner();
+    }
+    while(playerScore || computerScore < 5)
+   }
+playGame();
